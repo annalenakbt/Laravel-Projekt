682bfeaa900b16724a50a9d70c4550a511fe43af
annalenakbt <94553076+annalenakbt@users.noreply.github.com>
2022-11-21 12:31:01 +0100

    Einbauen der Registrieren Unterseite

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EinkauflisteController;
-use App\Http\Controller\UserController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
